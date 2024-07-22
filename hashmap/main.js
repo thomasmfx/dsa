99308c39c96cf4698fe804c19a3fec9a54ef3928
thomasmfx <thomasmoisesf@gmail.com>
2024-07-22 16:57:23 -0300

    feat: Add function to keep track of the capacity/size of an array
    In this case, the map HashMap size

--- hashmap/main.js
+++ hashmap/main.js
@@ -1,12 +1,16 @@
 import LinkedList from "../linked-lists/main.js";
 
 function generateBuckets(arr) {
-  let mapSize = arr.length;
+  let mapSize = capacity(arr)
   if (mapSize === 0) mapSize = 16;
 
   for(let i = 0; i < mapSize; i++) {
     arr.push(new LinkedList());
   }
+}
+
+function capacity(arr) {
+  return arr.length
 }
 
 class HashMap {
@@ -18,7 +22,7 @@ class HashMap {
   hash(key) {
     let hashCode = 0;
   
-    const mapSize = this.map.length;
+    const mapSize = capacity(this.map)
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % mapSize;
