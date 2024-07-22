3d15a0b98f1e1a5979f14e54faf2104e943e62af
thomasmfx <thomasmoisesf@gmail.com>
2024-07-22 16:51:24 -0300

    feat: Add function 'Hash' to... Well, hash
    Also modified the buckets generator to always generate the double of buckets of the current map size

--- hashmap/main.js
+++ hashmap/main.js
@@ -1,16 +1,31 @@
 import LinkedList from "../linked-lists/main.js";
 
 function generateBuckets(arr) {
-  for(let i = 0; i < 16; i++) {
+  let mapSize = arr.length;
+  if (mapSize === 0) mapSize = 16;
+
+  for(let i = 0; i < mapSize; i++) {
     arr.push(new LinkedList());
-  };
-};
+  }
+}
 
 class HashMap {
   constructor() {
     this.map = [];
     generateBuckets(this.map);
-  };
+  }
+
+  hash(key) {
+    let hashCode = 0;
+  
+    const mapSize = this.map.length;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % mapSize;
+    }
+  
+    return hashCode;
+  }
 };
 
 const test = new HashMap();
