ce74d3f4b65f139680be1114695d7453fa48918d
thomasmfx <thomasmoisesf@gmail.com>
2024-07-22 20:36:16 -0300

    refactor: Change structure of object returned from 'createNode'
    To avoid key-property colisions like a key named 'next'

--- hashmap/main.js
+++ hashmap/main.js
@@ -5,7 +5,7 @@ function generateBuckets(arr) {
   if (mapSize === 0) mapSize = 16;
 
   for(let i = 0; i < mapSize; i++) {
-    arr.push([new LinkedList()]);
+    arr.push(new LinkedList);
   }
 }
 
@@ -27,10 +27,21 @@ function loadFactor(arr) {
   if (entries > mapSize * factor) return generateBuckets(arr);
 }
 
+function createNode(key, value) {
+  let node = {
+    key: key,
+    value: value,
+    next: null
+  }
+  return node;
+}
+
 class HashMap {
   constructor() {
     this.map = [];
     generateBuckets(this.map);
+    // this.map[0].head = createNode('Carlos', 'Smith')
+    // this.map[0].head.next = createNode('Carlos', 'Smith')
   }
 
   hash(key) {
@@ -38,14 +49,15 @@ class HashMap {
   
     const mapSize = capacity(this.map)
     const primeNumber = 31;
+    // key = 'Carlos'
     for (let i = 0; i < key.length; i++) {
       hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % mapSize;
     }
   
     return hashCode;
   }
-};
+}
 
 const test = new HashMap();
-
-console.log(test)
+console.log(test.map)
+console.log(createNode('Carlos', 'Smith'))
