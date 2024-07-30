bd54de9a895118bb7ee8ae1db99c97e8ce47d296
thomasmfx <thomasmoisesf@gmail.com>
2024-07-29 23:40:21 -0300

    feat: Finish 'buildTree' function
    Creating BST properly

--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -2,41 +2,38 @@ import { Node, Tree } from './classes.js';
 import formatArray from './formatData.js';
 
 let sample = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-// let formated = [1, 3, 4, 5, 7,     8,     9, 23, 67, 324, 6345];
+// let formated = [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345];
 
 function buildTree(array) {
-  let copy = formatArray(array);
-  
-  let mid = parseInt(copy.length / 2); 
-  let left = copy.slice(0, mid);
-  let right = copy.slice(mid + 1);
-  
-  let node = new Node(copy[mid]);
-  
-  if (left.length > right.length) return new Node(copy.shift());
+  let start = 0;
+  let end = array.length - 1; 
+  let mid = (start + end) / 2;
 
-  node.setLeft(buildTree(left));
-  node.setRight(buildTree(right));
+  if (start > end) return null;
+
+  array = formatArray(array);
+  let node = new Node(array.splice(mid, 1));
+
+  node.setLeft(buildTree(array.slice(start, mid)));
+  node.setRight(buildTree(array.slice(mid, end)));
 
   return node;
 };
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node === null) return;
-
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '| ' : ' '}`, false);
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
   };
-  console.log(`${prefix}${isLeft ? '- ' : '_ '}${node.data}`);
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  };
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
   if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? ' ' : '| '}`, true);
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   };
 };
 
 let tree = new Tree(sample);
-
-// prettyPrint(tree.root);
-
-console.log(tree)
+prettyPrint(tree.root);
 
 export { buildTree };
