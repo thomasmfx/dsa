8ec8fc59b9b04d86d0a20ef885dd784310062637
thomasmfx <thomasmoisesf@gmail.com>
2024-08-02 21:39:31 -0300

    feat: Update driver script and add BST manipulation test

--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -1,7 +1,7 @@
 import { Node, Tree } from './classes.js';
 import formatArray from './formatData.js';
 
-let sample = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// let sample = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 function buildTree(array) {
   let start = 0;
@@ -35,49 +35,64 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 function generateRandomList(size) {
   let randomList = [];
   for (let i = 0; i < size; i++) {
-      randomList.push(Math.floor(Math.random() * 100));
+    randomList.push(Math.floor(Math.random() * 100));
   };
   return randomList;
 };
 
-function onlyData(obj) {
-  return obj.data;
-};
+let tree = new Tree(generateRandomList(16));
+prettyPrint(tree.root);
+
+// Tree Manipulation test - uncomment to run
+
+// console.log('\n');
+// console.log('Find 4: ', tree.find(4));
+// console.log('Height of 7: ', tree.height(tree.find(7)));
+// console.log('Depth of 5: ',tree.depth(tree.find(5)));
+// console.log('\n');
+// console.log('Insert 10 \n');
+// tree.insert(10);
+// prettyPrint(tree.root);
+// console.log('Delete 9 \n');
+// tree.deleteItem(9);
+// prettyPrint(tree.root);
 
 
-// Driver script
-let tree = new Tree(generateRandomList(10));
+// Driver script - uncomment to run
 
-prettyPrint(tree.root);
-console.log('\n');
-// Confirm balance
-console.log('Tree is balanced: ', tree.isBalanced());
-// Print all elements in different orders
-console.log('\n');
-console.log('Level-order: ', tree.levelOrder(onlyData));
-console.log('Pre-order: ', tree.preOrder(onlyData));
-console.log('Post-order: ', tree.postOrder(onlyData));
-console.log('Inorder: ', tree.inorder(onlyData));
-console.log('\n');
-// Unbalance tree
-tree.insert(150);
-tree.insert(200);
-tree.insert(250);
-// Confirm unbalance
-prettyPrint(tree.root);
-console.log('\n');
-console.log('Tree is balanced: ', tree.isBalanced());
-// Balance tree
-console.log('Rebalanced tree: ');
-console.log('\n');
-tree.rebalance();
-prettyPrint(tree.root);
-// Print all elements in different orders
-console.log('\n');
-console.log('Level-order: ', tree.levelOrder(onlyData));
-console.log('Pre-order: ', tree.preOrder(onlyData));
-console.log('Post-order: ', tree.postOrder(onlyData));
-console.log('Inorder: ', tree.inorder(onlyData));
-console.log('\n');
+// function callback(obj) {
+//   return obj.data;
+// };
+
+// console.log('\n');
+// // Confirm balance
+// console.log('Tree is balanced: ', tree.isBalanced());
+// // Print all elements in different orders
+// console.log('\n');
+// console.log('Level-order: ', tree.levelOrder(callback));
+// console.log('Pre-order: ', tree.preOrder(callback));
+// console.log('Post-order: ', tree.postOrder(callback));
+// console.log('Inorder: ', tree.inorder(callback));
+// console.log('\n');
+// // Unbalance tree
+// tree.insert(150);
+// tree.insert(200);
+// tree.insert(250);
+// // Confirm unbalance
+// prettyPrint(tree.root);
+// console.log('\n');
+// console.log('Tree is balanced: ', tree.isBalanced());
+// // Balance tree
+// console.log('Rebalanced tree: ');
+// console.log('\n');
+// tree.rebalance();
+// prettyPrint(tree.root);
+// // Print all elements in different orders
+// console.log('\n');
+// console.log('Level-order: ', tree.levelOrder(callback));
+// console.log('Pre-order: ', tree.preOrder(callback));
+// console.log('Post-order: ', tree.postOrder(callback));
+// console.log('Inorder: ', tree.inorder(callback));
+// console.log('\n');
 
 export { buildTree };
